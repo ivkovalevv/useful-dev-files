07be2b52723eb6f28045c4eae37b40f40cc7ea43
ivkovalevv <127996062+ivkovalevv@users.noreply.github.com>
2025-10-27 08:59:49 +0300

    Delete Before After block (HTML, CSS, JS) directory

--- Before After block (HTML, CSS, JS)/main.js
+++
@@ -1,6 +0,0 @@
-const slider = document.querySelector('.slider');
-const range = document.querySelector('.slider__range-js');
-
-range.addEventListener('input', () => {
-  slider.style.setProperty('--value', range.value + '%');
-});
